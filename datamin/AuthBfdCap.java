4dcca8c28494c1c9b808a0ebd1738af4f81d0e2d
ashhashhete <ashhashhete3945@gmail.com>
2025-03-25 17:59:45 +0530

    survey_app-407-main\archive\java\com\igenesys

---
+++ datamin/AuthBfdCap.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.igenesys.datamin;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+/**
+ * @author vineet
+ *
+ */
+public interface AuthBfdCap {
+	
+	void updateImageView(final ImageView imgPreview, final Bitmap previewBitmap, String message, final boolean flagComplete, int captureError);
+	//public void getresponse(String res);
+	//void setQlyFinger(final int qly,boolean flagComplete);
+	
+}
